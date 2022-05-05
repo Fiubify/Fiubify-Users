4f4e6b0891d1ef702867f9286cdebd67e6199c27
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-05-05 17:00:24 -0300

    prueba 0.2 coverage con tests

--- app/test_and_coverage.sh
+++ app/test_and_coverage.sh
@@ -3,6 +3,6 @@
 npm test --coverage
 
 apk --no-cache add curl
-curl -Os https://uploader.codecov.io/latest/linux/codecov
+curl -Os https://uploader.codecov.io/latest/alpine/codecov
 chmod +x codecov
 ./codecov -t ${CODECOV_TOKEN} -s ./coverage -R ./..
