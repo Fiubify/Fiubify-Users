fb0b1eea8843546167bd6d08094245dcb530b43c
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-05-05 17:31:54 -0300

    prueba 0.7 coverage con tests

--- app/test_and_coverage.sh
+++ app/test_and_coverage.sh
@@ -5,4 +5,4 @@ npm test
 apk --no-cache add curl
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 chmod +x codecov
-./codecov -t ${CODECOV_TOKEN} -s ./coverage -R ./
+CODECOV_SLUG=Fiubify/Fiubify-Users ./codecov -C ${CODECOV_SHA} -t ${CODECOV_TOKEN} -s ./coverage -R ./
