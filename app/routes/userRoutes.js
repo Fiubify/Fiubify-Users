d5740aecaeed2db30f9b389391a02a164ce65dd6
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-05-14 15:58:39 -0300

    Añadido el end-point GET user by uid

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userControllers = require('../controllers/userControllers');
 
 router.get('/', userControllers.getAllUsers);
+router.get('/:uid', userControllers.getUser);
 router.patch('/block/:id', userControllers.blockUser);
 router.patch('/unblock/:id', userControllers.unblockUser);
 
