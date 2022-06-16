dfc481c0c31cc5b8ffa05568ae7473e344a30927
pablo.berrotaran <pberrotaran@keepcon.com>
2022-06-16 00:39:16 -0300

    Lint

--- app/services/payments.js
+++ app/services/payments.js
@@ -1,8 +1,9 @@
 const axios = require("axios")
 
 const createUserWallet = async () => {
+  var response
   try {
-    const response = await axios.post("https://fiubify-payments-staging.herokuapp.com/wallets/", {})
+    response = await axios.post("https://fiubify-payments-staging.herokuapp.com/wallets/", {})
   } catch (error) {
     console.log(error)
     return
