88791ebabc6ade74c30aec6a6950a7a6ad38a745
pablo.berrotaran <pberrotaran@keepcon.com>
2022-06-15 23:45:21 -0300

    Fix payments service POST call

--- app/services/payments.js
+++ app/services/payments.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 
 const createUserWallet = async () => {
-  const response = await axios.post("https://fiubify-payments-staging.herokuapp.com/wallets/")
+  const response = await axios.post("https://fiubify-payments-staging.herokuapp.com/wallets/", {})
   return response.data.address
 }
 
