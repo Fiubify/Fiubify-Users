4c6f37dc613fd8ff0aef3b3e7b65a63e6ae38cc6
RamosFe <fedearamos97@gmail.com>
2022-06-17 22:01:54 -0300

    Added validation for uid.

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -6,6 +6,7 @@ const authControllers = require("../controllers/authControllers");
 router.post("/register-email", authControllers.createUserWithEmailAndPassword);
 router.post("/register-provider", authControllers.createUserWithProvider);
 router.post("/validate", authControllers.validateAuth);
+router.post("validate/uid", authControllers.validateUid);
 router.post("/validate/admin", authControllers.validateAdmin);
 router.post("/validate/user", authControllers.validateUserWithToken);
 router.post("/validate/users", authControllers.validateUsersWithToken); // Used for validating multiple ids
